a2ab74be639f3f2697accc5505a1ffb22c6247a7
Dafterfly <17124333+Dafterfly@users.noreply.github.com>
2023-11-08 20:36:26 +0200

    Fix for loading products in existing tabs
    Make the button appear no matter if the product was loaded in an existing tab or in a new tab
    Also changed the button color

--- content.js
+++ content.js
@@ -1,40 +1,51 @@
-// content.js
-
 function extractProductIdFromUrl(url) {
-	const urlParts = url.split('/');
-	const productIdIndex = urlParts.findIndex(part => part.startsWith('PLID'));
-	if (productIdIndex !== -1) {
-		return urlParts[productIdIndex];
-	}
-	return null;
+    const urlParts = url.split('/');
+    const productIdIndex = urlParts.findIndex(part => part.startsWith('PLID'));
+    if (productIdIndex !== -1) {
+        return urlParts[productIdIndex];
+    }
+    return null;
 }
 
-function addServalTrackerButton() {
-	// Check if the current URL matches the Takealot product page pattern
-	const productId = extractProductIdFromUrl(window.location.href);
-	
-	if (productId !== null) {	
-	//if (true){
-		// Create a button element
-		const button = document.createElement('a');
-		button.innerText = 'Track on Serval Tracker';
-		button.href = "https://www.servaltracker.com/products/"+productId;
-				
-		// Style the button
-		button.style.display = 'block';
-		button.style.marginTop = '10px';
-		button.style.padding = '10px';
-		button.style.backgroundColor = '#4CAF50';
-		button.style.color = 'white';
-		button.style.textAlign = 'center';
-		button.style.textDecoration = 'none';
-		button.style.borderRadius = '5px';
-		
-		// Insert the button after the product title
-		const titleElement = document.querySelector('.product-title');
-		titleElement.parentNode.insertBefore(button, titleElement.nextSibling);
-	}
+function addButton() {
+    
+    const productId = extractProductIdFromUrl(window.location.href);
+    
+    if (productId !== null) {
+        // Create a button element
+        const button = document.createElement('a');
+        button.innerText = 'Track on Serval Tracker';
+        button.href = "https://www.servaltracker.com/products/" + productId;
+        
+        // Style the button
+        button.style.display = 'block';
+        button.style.marginTop = '10px';
+        button.style.padding = '10px';
+        button.style.backgroundColor = 'gold';
+        button.style.color = 'black';
+        button.style.textAlign = 'center';
+        button.style.textDecoration = 'none';
+        button.style.borderRadius = '5px';
+        
+        // Insert the button after the product title
+        const titleElement = document.querySelector('.product-title');
+        if (titleElement) {
+            if (titleElement.nextElementSibling.tagName !== 'A') {
+                titleElement.parentNode.insertBefore(button, titleElement.nextSibling);
+            }
+        }
+    }
 }
 
-// Wait a bit for the entire product page to load before adding the button
-setTimeout(addServalTrackerButton, 2000);
+function callback(mutationsList, observer) {
+	setInterval(addButton, 5000);
+}
+
+
+// Create a MutationObserver object and pass the callback into the constructor
+var observer = new MutationObserver(callback);
+
+// Start observing the DOM changes
+observer.observe(document.body, { childList: true, subtree: true });
+
+
